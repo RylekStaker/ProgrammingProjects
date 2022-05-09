dff425bb342ed150d8a09ea3c855af99faac05ed
RylekStaker <89046770+RylekStaker@users.noreply.github.com>
2022-05-09 08:00:45 -0600

    Add files via upload

---
+++ src/drawMan.h
@@ -0,0 +1,17 @@
+#ifndef DRAWMAN_H
+#define DRAWMAN_H
+#include <iostream>
+
+using namespace std;
+
+extern int tries;
+extern string guess1;
+extern string guess2;
+extern string guess3;
+extern string guess4;
+extern string guess5;
+extern string guess6;
+
+void drawMan();
+
+#endif
